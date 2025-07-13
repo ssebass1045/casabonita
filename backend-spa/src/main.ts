ef85521392228e669eea812e52aab0644688ca1b
ssebass1045 <sebas.22.26@hotmail.com>
2025-07-13 15:21:11 -0500

    listo para subir

--- backend-spa/src/main.ts
+++ backend-spa/src/main.ts
@@ -18,12 +18,12 @@ async function bootstrap() {
   app.enableCors();
 
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
 
   console.log('Iniciando servidor...');
   console.log("-----------------------");
   console.log('Server started on port ' + port);
-  console.log('Visit http://localhost:' + port);
+  //console.log('Visit http://localhost:' + port);
   console.log("-----------------------");
   console.log(`conectado a la base de datos "${process.env.DATABASE_NAME}" puerto ${process.env.DATABASE_PORT}`);
   console.log("...")
